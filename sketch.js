88b2df956484481e6f585c9c3e725a9f459b0436
tanmayGulabani <75249050+tanmayGulabani@users.noreply.github.com>
2021-01-23 09:36:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var dustbin1,dustbin2,dustbin3,ground;
+function setup() {
+	createCanvas(1200, 700);
+	engine = Engine.create();
+	world = engine.world;
+	ground = new Ground(600,600,1200,20);
+	dustbin=new Dustbin();
+   
+   dustbin4 = new Dustbin(610,550,20,100);
+	dustbin5 = new Dustbin(500,590,200,20);
+   dustbin6 = new Dustbin(400,550,20,100);
+   
+  paper = new Paper(200,575,25);
+	Engine.run(engine);
+  
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ground.display();
+  dustbin.display();
+
+  dustbin4.display();
+  dustbin5.display();
+  dustbin6.display();
+ paper.display();
+}
+function keyPressed() {
+   if(keyCode === UP_ARROW) {
+      Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85})
+   }if(keyCode === DOWN_ARROW) {
+      Matter.Body.applyForce(paper.body,paper.body.position,{x:-65,y:65})
+   }
+
+}
